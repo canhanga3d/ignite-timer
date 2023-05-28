928fb3e0ce1a9104aa759f9c9bd92ea2ebb626af
Domingos Canhanga <canhanga96@gmail.com>
2023-05-28 12:14:17 +0100

    :rocket: Commit: Commit: Commit: Application structure - Theme typing

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import 'styled-components';
+import { defaultTheme } from './../styles/themes/defaultTheme';
+type ThemeType = typeof defaultTheme;
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
