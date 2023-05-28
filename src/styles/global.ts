01acd2330d4671ba00c53baf5192a3c1d19db9eb
Domingos Canhanga <canhanga96@gmail.com>
2023-05-28 13:34:53 +0100

    :rocket: Application structure - global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
+*, *::after, *::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  background-color: #333;
+  color: #fff;
+
+}
+`;
