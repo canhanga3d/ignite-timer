ba17e7f4e83b2954cabf74e9bea0fcaec4392523
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-05-30 15:57:49 +0100

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    '@rocketseat/eslint-config/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
+
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    '@typescript-eslint/no-empty-interface': 'off',
   },
-}
+};
--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components';
 import { defaultTheme } from './../styles/themes/defaultTheme';
 type ThemeType = typeof defaultTheme;
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
+    export interface DefaultTheme extends ThemeType {}
 }
