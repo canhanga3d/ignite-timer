5aac3c3eb13b3c7c5f2219f5de82c09e94980735
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-06-08 14:14:37 +0100

    🚀 Pages and Routes - Layout Styling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,16 +4,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
     '@rocketseat/eslint-config/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh', 'prettier'],
-
   rules: {
     'react-refresh/only-export-components': 'warn',
     'prettier/prettier': ['error', { singleQuote: true, semi: true }],
     '@typescript-eslint/no-empty-interface': 'off',
+    ' prettier/prettier': false,
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components';
-
 import { defaultTheme } from './styles/themes/defaultTheme';
 import { GlobalStyle } from './styles/global';
 import { Router } from './Router';
