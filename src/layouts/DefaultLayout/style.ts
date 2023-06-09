b0328812c3332b1c411a0e52f27b6750f794333e
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-06-09 17:30:56 +0100

    🚀 Pages and Routes - fix the LayoutContainer

--- src/layouts/DefaultLayout/style.ts
+++ src/layouts/DefaultLayout/style.ts
@@ -2,11 +2,15 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
   width: min(95%, 118.4rem);
-  height: calc(100vh - 16rem);
-  margin: 8rem auto;
-  padding: 2.5rem ;
+  height: calc(100vh - 6rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
   display: flex;
   flex-direction: column;
+  @media screen and(min-width:600px) {
+    height: calc(100vh - 16rem);
+    margin: 8rem auto;
+  }
 `;
